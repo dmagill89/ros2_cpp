d295a1937ebba8e1f422c4dd71ae8da65224d802
David Magill <dmagill89@gmail.com>
2023-08-20 13:30:16 -0700

    update launch file to also launch spped calc node and updated speed calc node to support parameters for the wheel radius.

--- src/cpp_pkg/launch/rpm_node.launch.py
+++ src/cpp_pkg/launch/rpm_node.launch.py
@@ -12,8 +12,20 @@ def generate_launch_description():
               { "rpm_value": 5.0 }
           ]
         ),
+        Node(
+          package="cpp_pkg",
+          executable="speed_calc",
+          name="speed_calc_sub_node",
+          parameters=[
+              { "wheel_radius": 0.185 }
+          ]
+        ),
         ExecuteProcess(
           cmd=['ros2', 'topic', 'list'],
+          output='screen'
+        ),
+        ExecuteProcess(
+          cmd=['ros2', 'topic', 'echo', '/speed'],
           output='screen'
         )
     ])
--- src/cpp_pkg/src/speed_calc.cpp
+++ src/cpp_pkg/src/speed_calc.cpp
@@ -4,13 +4,14 @@
 #include <functional>
 #include <math.h>
 
-const float WHEEL_RADIUS = 12.5 /100; // centimeters to meters 
+const double WHEEL_DEFAULT_RADIUS = 12.5 /100; // centimeters to meters 
 
 class SpeedCalcSubNode : public rclcpp::Node
 {
   public:
     SpeedCalcSubNode(): Node("speed_calc_sub_node") 
     {
+        this->declare_parameter<double>("wheel_radius", WHEEL_DEFAULT_RADIUS);
         rpm_subscription_ = this->create_subscription<std_msgs::msg::Float64>(
             "rpm",
             10,
@@ -32,9 +33,10 @@ class SpeedCalcSubNode : public rclcpp::Node
     void calculate_and_pub_speed(const std_msgs::msg::Float64 &rpm_msg) const
     {
         auto speed_msg = std_msgs::msg::Float64();
-
+        rclcpp::Parameter radius_param_obj = this->get_parameter("wheel_radius");
+        auto wheel_radius = radius_param_obj.as_double();
         // Speed m/s = RPM [rev/min] * wheel circumference [meters/rev]/ 60 [seconds/min]
-        speed_msg.data = rpm_msg.data * (2 * WHEEL_RADIUS * M_PI) / 60;
+        speed_msg.data = rpm_msg.data * (2 * wheel_radius * M_PI) / 60;
 
         speed_publisher_->publish(speed_msg);
 
